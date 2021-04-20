7f54be496f56c931db4448454746106156adb708
Paolo Tiu <paolotiu17@gmail.com>
2021-04-20 22:44:52 +0800

    added spacer for hover bg

--- src/Tree/File.tsx
+++ src/Tree/File.tsx
@@ -2,15 +2,19 @@ import styled from '@emotion/styled';
 import React from 'react';
 
 const StyledItem = styled.div`
-    padding-left: calc(var(--item-padding-left) + var(--chevron-width));
+    padding-left: var(--item-padding-left);
+    cursor: pointer;
     .__kreme-file-label {
         padding: 0.2em 0.3em;
+
+        .spacer {
+            padding-left: calc(var(--chevron-width));
+        }
         display: flex;
         align-items: center;
         span {
             margin-left: var(--margin-left);
         }
-
         :hover {
             background-color: rgba(55, 53, 47, 0.08);
         }
@@ -34,6 +38,7 @@ const File = ({ onFileClick, name, id }: Props) => {
     return (
         <StyledItem role='button' tabIndex={0} onClick={handleClick} onKeyPress={handleClick}>
             <div className='__kreme-file-label'>
+                <div className='spacer' />
                 <span>{name}</span>
             </div>
         </StyledItem>
