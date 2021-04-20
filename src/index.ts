58b81e85fc105e9d0416658d5e06e9b6c2dc721c
Paolo Tiu <paolotiu17@gmail.com>
2021-04-20 22:45:27 +0800

    added exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export * from './TestComponent/TestComponent';
+export { default as CircleProgress } from './CircleProgress/CircleProgress';
+export { default as Tree } from './Tree/Tree';
+export { default as KremeProvider } from './shared/KremeProvider';
