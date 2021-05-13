5312d22b7645330d071473411b986669a28bc2a1
Paolo Tiu <paolotiu17@gmail.com>
2021-05-13 13:52:05 +0800

    No default font

--- src/shared/KremeProvider.tsx
+++ src/shared/KremeProvider.tsx
@@ -1,17 +1,11 @@
-import { Global, ThemeProvider } from '@emotion/react';
+import { ThemeProvider } from '@emotion/react';
 import React from 'react';
-import { globalStyles } from './global';
 import theme from './theme';
 
 interface Props {
     children: React.ReactNode;
 }
 
-const KremeProvider = ({ children }: Props) => (
-    <ThemeProvider theme={theme}>
-        <Global styles={globalStyles} />
-        {children}
-    </ThemeProvider>
-);
+const KremeProvider = ({ children }: Props) => <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 
 export default KremeProvider;
