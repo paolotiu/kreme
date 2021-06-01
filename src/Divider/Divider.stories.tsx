ffb69358d1bed4911b4b026f8a5f69418ea2fb71
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2021-06-01 15:03:58 +0800

    added divider

---
+++ src/Divider/Divider.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Divider, Props } from './Divider';
+import { Meta } from '@storybook/react';
+
+export default {
+    title: 'Example/Divider',
+    component: Divider,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+        textColor: { control: 'color' },
+        lineColor: { control: 'color' },
+        lineThickness: { control: 'text' },
+        lineStyle: {
+            control: {
+                type: 'select',
+                options: [
+                    'dotted',
+                    'dashed',
+                    'solid',
+                    'double',
+                    'groove',
+                    'ridge',
+                    'inset',
+                    'outset',
+                    'none',
+                    'hidden',
+                ],
+                default: 'solid',
+            },
+        },
+    },
+    args: {
+        text: 'Hello World',
+        lineStyle: 'solid',
+    },
+} as Meta;
+
+export const Default = (args: Props) => <Divider {...args} />;
---
+++ src/Divider/Divider.tsx
@@ -0,0 +1,52 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import { CSSBorder } from '../types';
+
+interface LineProps {
+    lineColor: string;
+    lineThickness: string;
+    lineStyle: string;
+}
+const Line = styled.p<LineProps>`
+    width: 100%;
+    text-align: center;
+    border-top: ${(props) => `${props.lineThickness} ${props.lineStyle} ${props.lineColor}`};
+    line-height: 0;
+    margin: 10px 0 20px;
+`;
+const Text = styled.span<{ backgroundColor: string; textColor: string }>`
+    background: ${(props) => props.backgroundColor};
+    color: ${(props) => props.textColor};
+    padding: 0 10px;
+`;
+
+export interface Props {
+    text: string;
+    backgroundColor?: string;
+    textColor?: string;
+    lineColor?: string;
+    lineStyle?: CSSBorder;
+    /** 
+    Any valid css unit
+  */
+    lineThickness?: string;
+}
+
+export const Divider = ({
+    text,
+    backgroundColor = 'white',
+    textColor = 'black',
+    lineColor = 'black',
+    lineThickness = '1px',
+    lineStyle = 'solid',
+}: Props) => {
+    return (
+        <div>
+            <Line lineColor={lineColor} lineThickness={lineThickness} lineStyle={lineStyle}>
+                <Text backgroundColor={backgroundColor} textColor={textColor}>
+                    {text}
+                </Text>
+            </Line>
+        </div>
+    );
+};
